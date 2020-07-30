f0e2173ab85b038fab4b342f27d404456f447bed
JanWielgus <jan.wielgus12@gmail.com>
2020-07-30 15:18:39 +0200

    Created IByteType interface

---
+++ IByteType.h
@@ -0,0 +1,23 @@
+/**
+ * @file IByteType.h
+ * @author Jan Wielgus
+ * @date 2020-07-30
+ * 
+ */
+
+#ifndef IBYTETYPE_H
+#define IBYTETYPE_H
+
+
+class IByteType
+{
+public:
+    virtual ~IByteType() {}
+
+    virtual uint8_t* byteArray() = 0;
+    virtual const uint8_t* byteArray() const = 0;
+    virtual uint8_t byteSize() = 0;
+};
+
+
+#endif
